7c654d9264567f0c3af88b078fd4b6ed1c1c06f5
im-amartsynkevich <88032890+im-amartsynkevich@users.noreply.github.com>
2022-11-22 21:16:21 +0300

    firestore setup

---
+++ src/firebase/config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBzFNuZpZFmo00ODltBPhHPVDiiIC64GLE",
+    authDomain: "auction-1459b.firebaseapp.com",
+    databaseURL: "https://auction-1459b-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "auction-1459b",
+    storageBucket: "auction-1459b.appspot.com",
+    messagingSenderId: "692768848863",
+    appId: "1:692768848863:web:9603092f62839cd6d8a26a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
