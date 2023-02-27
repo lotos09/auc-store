561181b8d6bb205eba074eaf27e1397a5fedf57a
im-amartsynkevich <88032890+im-amartsynkevich@users.noreply.github.com>
2023-02-27 18:21:41 +0300

    clean up

--- src/components/AccountDetailsPage/AccountDetailsPage.js
+++ src/components/AccountDetailsPage/AccountDetailsPage.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useContext, useMemo } from "react";
+import React from "react";
 import { Paper } from "@mui/material";
 import { paperStyle } from "../../constants/styles";
 
